0b94a9c18777fdfec4c04951fdb78b9a74038aa2
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-21 18:52:04 -0500

    Understanding the CSS module, imporing css module in JSX

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,10 +1,10 @@
-import "./userProfile.css";
+import styles from "./userProfile.module.css";
 
 export default function userProfile() {
   return (
     <>
       <section>
-        <ul className="user-details">
+        <ul className={`${styles.userDetails} ${styles.border}`}>
           <li>FirstName: John</li>
           <li>Last Name: Doe</li>
           <li>Skills: React, javascript</li>
