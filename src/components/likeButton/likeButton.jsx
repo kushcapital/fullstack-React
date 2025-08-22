e11e97e1b076f1371d3a7db919e17a4ec298a074
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-21 22:03:12 -0500

    Added first Hooks in the project i.e useState

--- src/components/likeButton/likeButton.jsx
+++ src/components/likeButton/likeButton.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
+
 export default function likeButton() {
-  let likes = 0;
+  let [likes, setlikes] = useState(0);
+
   function countLIkes(e) {
-    likes ++ ;
+    setlikes(likes + 1);
     console.log(likes);
-    console.log(e)
   }
   return <button onClick={countLIkes}> {likes} Likes</button>;
 }
