13241923d1879ca2ee00513bbd3c3ba07aae7453
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-22 18:38:01 -0500

    Cleaned up to start of frontend for to-do-list app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import UserProfile from "./components/userProfile/userProfile.jsx";
 
-const user = {
-  firstName: "john",
-  lastName: "Doe",
-  skills: "node.js and React",
-};
-const user2 = {
-  firstName: "Mark",
-  lastName: "Doe",
-  skills: "HTML and CSS",
-};
 
 function App() {
-  return (
-    <>
-      <UserProfile
-        firstName={user.firstName}
-        lastName={user.lastName}
-        skills={user.skills}
-      />
-      <UserProfile
-        firstName={user2.firstName}
-        lastName={user2.lastName}
-        skills={user2.skills}
-      />
-    </>
-  );
+  return <><p>Hello world</p></>;
 }
 
 export default App;
