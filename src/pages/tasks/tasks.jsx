11b203cf16c4341572e6b36656cace38ee794e01
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-23 22:31:06 -0500

    Created Task Counter Component

--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -1,3 +1,5 @@
+import { TaskCounter } from "@/components/tasksCounter/tasksCounter.jsx";
+
 export default function Tasks() {
   return (
     <section className="flex flex-ro w-full p-4 gap-8 ">
@@ -5,9 +7,9 @@ export default function Tasks() {
         <div className="flex flex-col w-4/5 p-4">
           <h1 className="text-white font-bold text-2xl mb-8">Tasks as on: Wednesday, 1 jan 2025</h1>
           <div className="flex justify-around">
-            <p>Counter 1</p>
-            <p>Counter 2</p>
-            <p>Counter 3</p>
+            <TaskCounter/>
+            <TaskCounter/>
+            <TaskCounter/>
           </div>
         </div>
       </section>
