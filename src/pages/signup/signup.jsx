db1b07a2ee31e1811d801c6c240817e9a7fc3761
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-23 20:23:32 -0500

    setup singup page layout

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,3 +1,31 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function SignUp() {
-  return <>Signup page</>;
+  return (
+    <section className="flex flex-row w-full max-w-screen-xl min-h-screen justify-center items-center">
+      <div className="h-20 w-4/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
 }
