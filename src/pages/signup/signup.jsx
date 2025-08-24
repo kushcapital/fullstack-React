73af9dc82abc0b15ac46f4eed572c942d327a974
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-23 21:13:29 -0500

    created signup form for singup page complete

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -7,22 +7,36 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button.jsx";
+import { Link } from "react-router";
 
 export default function SignUp() {
   return (
     <section className="flex flex-row w-full max-w-screen-xl min-h-screen justify-center items-center">
-      <div className="h-20 w-4/12">
+      <div className=" w-4/12">
         <Card>
           <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
+            <CardTitle>Signup</CardTitle>
+            <CardDescription>
+              Create a new account to start creating tasks
+            </CardDescription>
             <CardAction>Card Action</CardAction>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <Input className="mb-4" type="text" placeholder="First Nmae" />
+            <Input className="mb-4" type="text" placeholder="Last Name" />
+            <Input className="mb-4" type="email" placeholder="Email" />
+            <Input className="mb-4" type="password" placeholder="Password" />
           </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
+          <CardFooter className="flex flex-row justify-between">
+            <p>
+              Already have an account ?{" "}
+              <Link to="/" className="hover:text-blue-500">
+                login Here
+              </Link>
+            </p>
+            <Button>Signup</Button>
           </CardFooter>
         </Card>
       </div>
