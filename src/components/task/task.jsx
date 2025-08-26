a014ac7a9e51f0aa256bc108de19e40983d4a1e5
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-26 12:40:27 -0500

    addes some props and improve the looks of priority icon

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -12,34 +12,57 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-export function Task() {
+export function Task(props) {
+  const {
+    title = "This is the dafault title",
+    description = "This is the dafault description",
+    status = "todo",
+    priority = "low",
+    dueDate,
+  } = props;
   return (
     <Card className="w-full mb-6 shadow-lg hover:shadow-xl transition-shadow duration-200 border-l-4 border-l-blue-500">
       <CardHeader className="flex flex-row justify-between items-start pb-1">
         <CardTitle className="basis-2/3 text-slate-800 font-semibold text-xl leading-tight">
-          Title of the task Title of the task Title of the task Title of the
-          task Title of the task
+          {title}
         </CardTitle>
         <div className="flex gap-2">
           <Badge
-            className="bg-slate-100 text-slate-700 border-slate-300"
+            className="bg-slate-700 border-slate-300"
             variant="outline"
           >
             1 jan, 2025
           </Badge>
-          <Badge
-            className="bg-blue-100 text-blue-700 border-blue-300"
-            variant="outline"
-          >
-            normal
-          </Badge>
+
+          {priority === "normal" && (
+            <Badge
+              className=" bg-blue-700 border-blue-300"
+              variant="outline"
+            >
+              {priority}
+            </Badge>
+          )}
+          {priority === "high" && (
+            <Badge
+              className=" bg-red-700 border-blue-300"
+              variant="outline"
+            >
+              {priority}
+            </Badge>
+          )}
+          {priority === "low" && (
+            <Badge
+              className="bg-green-700 border-blue-300"
+              variant="outline"
+            >
+              {priority}
+            </Badge>
+          )}
         </div>
       </CardHeader>
       <CardContent className="pt-1 pb-4">
         <CardDescription className="text-slate-600 text-sm leading-relaxed">
-          This is the description of the taask This is the description of the
-          taaskThis is the description of the taaskThis is the description of
-          the taaskThis is the description of the taaskThis is the description
+          {description}
         </CardDescription>
       </CardContent>
       <CardFooter className="flex justify-between items-center pt-4 border-t border-slate-100">
