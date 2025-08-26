c9d1f95bfaa0a62c3cd829fa78319453dfe37c57
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-26 13:38:59 -0500

    Added props to Task component  453

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -18,8 +18,16 @@ export function Task(props) {
     description = "This is the dafault description",
     status = "todo",
     priority = "low",
-    dueDate,
+    dueDate = new Date("2025-01-01T12:00:00.0002"),
   } = props;
+
+  let formattedDate =
+    dueDate?.toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    }) || "No date";
+
   return (
     <Card className="w-full mb-6 shadow-lg hover:shadow-xl transition-shadow duration-200 border-l-4 border-l-blue-500">
       <CardHeader className="flex flex-row justify-between items-start pb-1">
@@ -27,34 +35,22 @@ export function Task(props) {
           {title}
         </CardTitle>
         <div className="flex gap-2">
-          <Badge
-            className="bg-slate-700 border-slate-300"
-            variant="outline"
-          >
-            1 jan, 2025
+          <Badge className="bg-slate-700 border-slate-300" variant="outline">
+            {formattedDate}
           </Badge>
 
           {priority === "normal" && (
-            <Badge
-              className=" bg-blue-700 border-blue-300"
-              variant="outline"
-            >
+            <Badge className=" bg-blue-700 border-blue-300" variant="outline">
               {priority}
             </Badge>
           )}
           {priority === "high" && (
-            <Badge
-              className=" bg-red-700 border-blue-300"
-              variant="outline"
-            >
+            <Badge className=" bg-red-700 border-blue-300" variant="outline">
               {priority}
             </Badge>
           )}
           {priority === "low" && (
-            <Badge
-              className="bg-green-700 border-blue-300"
-              variant="outline"
-            >
+            <Badge className="bg-green-700 border-blue-300" variant="outline">
               {priority}
             </Badge>
           )}
@@ -68,6 +64,10 @@ export function Task(props) {
       <CardFooter className="flex justify-between items-center pt-4 border-t border-slate-100">
         <div className="flex items-center gap-3">
           <Switch
+            checked={status === "inProgress" ? true : false}
+            onCheckedChange={() => {
+              console.log("Swtiched changes");
+            }}
             id="in-progress"
             className="data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-slate-300 border-2 border-slate-200"
           />
