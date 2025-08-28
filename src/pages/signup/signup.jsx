d1501b923d218d83beab6716b7ccd144d08550a9
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-08-27 22:38:42 -0500

    installed react hook form and reactform.resolvers frrom ui.shadcn and installed form from shadcn; updated the validator for form of sign up for the frontend; minor changes on signup schema for error

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -7,11 +7,31 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button.jsx";
 import { Link } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { SignupSchema } from "../../schema/signup.schema.js";
 
 export default function SignUp() {
+  const form = useForm({
+    resolver: zodResolver(SignupSchema),
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <section className="flex flex-row w-full max-w-screen-xl min-h-screen justify-center items-center">
       <div className=" w-4/12">
@@ -23,21 +43,93 @@ export default function SignUp() {
             </CardDescription>
             <CardAction>Card Action</CardAction>
           </CardHeader>
-          <CardContent>
-            <Input className="mb-4" type="text" placeholder="First Nmae" />
-            <Input className="mb-4" type="text" placeholder="Last Name" />
-            <Input className="mb-4" type="email" placeholder="Email" />
-            <Input className="mb-4" type="password" placeholder="Password" />
-          </CardContent>
-          <CardFooter className="flex flex-row justify-between">
-            <p className="basis-1/2">
-              Already have an account?{" "}
-              <Link to="/" className="hover:text-blue-500">
-                login Here
-              </Link>
-            </p>
-            <Button>Signup</Button>
-          </CardFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <CardContent>
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          className="mb-4"
+                          type="text"
+                          placeholder="First Nmae"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          className="mb-4"
+                          type="text"
+                          placeholder="Last Nmae"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          className="mb-4"
+                          type="text"
+                          placeholder="Email"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          className="mb-4"
+                          type="password"
+                          placeholder="Password"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-row justify-between">
+                <p className="basis-1/2">
+                  Already have an account?{" "}
+                  <Link to="/" className="hover:text-blue-500">
+                    login Here
+                  </Link>
+                </p>
+                <Button>Signup</Button>
+              </CardFooter>
+            </form>
+          </Form>
         </Card>
       </div>
     </section>
--- src/schema/signup.schema.js
+++ src/schema/signup.schema.js
@@ -1,4 +1,4 @@
-import { email, z } from "zod";
+import { z } from "zod";
 
 const passwordValidation = new RegExp(
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
@@ -14,13 +14,13 @@ export const SignupSchema = z.object({
     .max(100, { message: "First name cannot be more than 100 characters" }),
   lastName: z
     .string()
-    .optional()
-    .max(100, { message: "Last name cannot be more than 100 characters" }),
-  email: z.email().string(),
+    .max(100, { message: "Last name cannot be more than 100 characters" })
+    .optional(),
+  email: z.string().email(),
   password: z
     .string()
-    .min(8, { message: "Paassword must be at least 8 characters." })
-    .RegExp(passwordValidation, {
+    .min(8, { message: "Password must be at least 8 characters." })
+    .regex(passwordValidation, {
       message:
         "Password must include one number, one uppercase letter, one lowercase letter, and one special character. ",
     }),
