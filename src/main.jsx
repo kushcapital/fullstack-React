e0056c0606475719febe7df2306131248775ed7d
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-09-17 16:36:48 -0500

    setup Transtack query

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./routes.jsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 // Get the root DOM element.
 const rootElement = document.getElementById("root");
@@ -12,7 +16,10 @@ const root = createRoot(rootElement);
 // Render the application within StrictMode.
 root.render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     {/* Provide the router configuration to the application */}
     <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen= {false}/>
+    </QueryClientProvider>
   </StrictMode>
 );
