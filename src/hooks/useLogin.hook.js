a4ac103ebaaa55d2aa266b17e3160124d91c2826
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-09-18 21:47:21 -0500

    Storing the accessToken as cookies in browser

--- src/hooks/useLogin.hook.js
+++ src/hooks/useLogin.hook.js
@@ -1,16 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
+import Cookies from "js-cookie";
 
 const loginUser = async (user) => {
-  const response = await fetch(
-    `${import.meta.env.VITE_API_URL}auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    }
-  );
+  const response = await fetch(`${import.meta.env.VITE_API_URL}auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
@@ -21,7 +19,7 @@ export function useLogin() {
   return useMutation({
     mutationFn: loginUser,
     onSuccess: (response) => {
-      console.log("User login successful", response);
+      Cookies.set("token", response.data.accessToken, { expires: 7 });
     },
     onError: (error) => {
       console.log("Error authenticating ", error);
