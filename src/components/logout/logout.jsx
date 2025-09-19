b1eabb49118a1deef3bf737224e604587be2f8a3
Rajeev Kushwaha <kushwaharajeev@outlook.com>
2025-09-19 18:50:23 -0500

    feature-logout added logout feature.

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -1,10 +1,23 @@
 import { Button } from "../ui/button.jsx";
 import { LogOut } from "lucide-react";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 export function Logout() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    Cookies.remove("token");
+    navigate("/");
+  }
   return (
     <div className="flex justify-end">
-      <Button variant="outline" size="icon" className="size-9">
+      <Button
+        onClick={handleClick}
+        variant="outline"
+        size="icon"
+        className="size-9"
+      >
         <LogOut />
       </Button>
     </div>
